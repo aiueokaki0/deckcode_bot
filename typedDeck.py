57b5c7c7f61230486c80a0bc6a9356fa615b536c
Okaki Aiue <tyogx0guil@gmail.com>
2020-10-14 21:32:56 +0900

    landmark

--- typedDeck.py
+++ typedDeck.py
@@ -6,6 +6,7 @@ class TypedDeck:
 		self.cards = self._sortCards(self.deck.cards)
 		self.units = list(filter(lambda card: card.cardType == "ユニット", self.cards))
 		self.spells = list(filter(lambda card: card.cardType == "スペル", self.cards))
+		self.landmarks = list(filter(lambda card: card.cardType == "ランドマーク", self.cards))
 		self.champions = list(filter(lambda card: card.superType == "チャンピオン", self.units))
 		self.followers = list(filter(lambda card: card.superType == "", self.units))
 		self.regions = set([card.region for card in self.cards])
